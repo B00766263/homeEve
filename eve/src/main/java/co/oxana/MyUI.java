64defefa142a73600a09631fce12f99d4a9fabda
Oxana <oxanka07@gmail.com>
2018-11-26 22:40:37 +0000

    change

--- eve/src/main/java/co/oxana/MyUI.java
+++ eve/src/main/java/co/oxana/MyUI.java
@@ -6,11 +6,8 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.*;
+
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -24,19 +21,58 @@ public class MyUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
 
-        Button button = new Button("press here");
-        button.addClickListener(e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
+       // final GridLayout layout = new GridLayout(2,2);//master layout
+        final VerticalLayout layout = new VerticalLayout();
+        HorizontalLayout panels = new HorizontalLayout();
+        GridLayout inputPanel = new GridLayout(1,2);//master layout
+        layout.setWidth("800"); 
+        VerticalLayout details = new VerticalLayout();
+        VerticalLayout cell1 = new VerticalLayout();
+        HorizontalLayout cell2 = new HorizontalLayout();//button
+        //VerticalLayout cell3 = new VerticalLayout();
+        //HorizontalLayout cell4 = new HorizontalLayout();
+        
+        //label at top content
+        Label label = new Label("Staff details");
+
+        //details contents
+
+        final TextField staffNum = new TextField();
+        staffNum.setCaption("Your staff number:");
+        
+        final TextField phone = new TextField();
+        phone.setCaption("Type your name here:");
+
+        final TextField name = new TextField();
+        name.setCaption("Phone number:");
+
+
+        //cell4 content
+        Button add = new Button("add");
+        add.addClickListener(e -> {
+            layout.addComponent(new Label(staffNum.getValue() 
+                                            + " " + name.getValue() + " " + phone.getValue()));
+
         });
-        
-        layout.addComponents(name, button);
-        
+
+        Button reset = new Button("Reset");
+        reset.addClickListener(e -> {
+           details.removeAllComponents();
+        });
+        //build layers
+
+        cell1.addComponents(staffNum, name, phone);
+        cell2.addComponents(add, reset);
+        inputPanel.addComponents(cell1, cell2);
+        panels.addComponents(inputPanel,details);
+        //panels.setComponentAlignment(inputPanel, Alignment.MIDDLE_LEFT);
+        //panels.setComponentAlignment(details, Alignment.MIDDLE_RIGHT);
+        layout.addComponents(label, panels);
+        layout.setSpacing(true);
+        //cell4.addComponent(add, reset);
+
+        //layout.addComponents(label, panels); 
         setContent(layout);
     }
 
@@ -45,3 +81,4 @@ public class MyUI extends UI {
     public static class MyUIServlet extends VaadinServlet {
     }
 }
+
